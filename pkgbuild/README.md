a25aa1f4e2d16a3854298cfdcb29c09b1ae39a9e
George Adams <george.adams@uk.ibm.com>
2018-11-20 22:31:28 +0000

    allow signing

--- pkgbuild/README.md
+++ pkgbuild/README.md
@@ -5,3 +5,9 @@
 ```bash
 ./pkgbuild.sh --major_version 8 --full_version 1.8.0_192 --input_directory /path/to/jdk --output_directory OpenJDK8U-jdk_x64_mac_hotspot_8u181b13.pkg
 ```
+
+## Sign the installer
+
+```bash
+--sign <certificate name>
+```
--- pkgbuild/pkgbuild.sh
+++ pkgbuild/pkgbuild.sh
@@ -7,6 +7,7 @@ while test $# -gt 0; do
       echo "--full_version           1.8.0_192>"
       echo "-i, --input_directory    path to extracted jdk>"
       echo "-o, --output_directory   name of the pkg file>"
+      echo "-s, --sign               sign the installer>"
       exit 0
       ;;
     --major_version)
@@ -29,6 +30,11 @@ while test $# -gt 0; do
       OUTPUT_DIRECTORY=$1
       shift
       ;;
+    -s|--sign)
+      shift
+      SIGN="$1"
+      shift
+      ;;
     *)
       break
       ;;
@@ -47,12 +53,13 @@ ln -nsf "${INPUT_DIRECTORY}/Contents/Home/lib/server/libjvm.dylib" "${INPUT_DIRE
 /usr/libexec/PlistBuddy -c "Set :JavaVM:JVMVendor AdoptOpenJDK" "${INPUT_DIRECTORY}/Contents/Info.plist"
 
 cat distribution.xml.tmpl  \
-		| sed -E "s/\\{major_version\\}/$MAJOR_VERSION/g" \
-		| sed -E "s/\\{full_version\\}/$FULL_VERSION/g" \
-    | sed -E "s/\\{file\\}/OpenJDK.pkg/g" \
->distribution.xml ; \
+  | sed -E "s/\\{major_version\\}/$MAJOR_VERSION/g" \
+  | sed -E "s/\\{full_version\\}/$FULL_VERSION/g" \
+  | sed -E "s/\\{file\\}/OpenJDK.pkg/g" \
+  >distribution.xml ; \
 
-/usr/bin/pkgbuild --root ${INPUT_DIRECTORY} --install-location /Library/Java/JavaVirtualMachines/adoptopenjdk-${MAJOR_VERSION}.jdk --identifier net.adoptopenjdk.${MAJOR_VERSION}.jdk --version ${FULL_VERSION} OpenJDK.pkg
-/usr/bin/productbuild --distribution distribution.xml --resources Resources --package-path OpenJDK.pkg ${OUTPUT_DIRECTORY}
+/usr/bin/pkgbuild --root ${INPUT_DIRECTORY} --install-location /Library/Java/JavaVirtualMachines/adoptopenjdk-${MAJOR_VERSION}.jdk --identifier net.adoptopenjdk.${MAJOR_VERSION}.jdk --version ${FULL_VERSION} --sign "${SIGN}" OpenJDK.pkg
+/usr/bin/productbuild --distribution distribution.xml --resources Resources  --sign "${SIGN}" --package-path OpenJDK.pkg ${OUTPUT_DIRECTORY}
+
 
 rm -rf OpenJDK.pkg
