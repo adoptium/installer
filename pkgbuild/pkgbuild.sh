b35817f99d9b357d39332ae041e6cea5aca4677c
George Adams <george.adams@uk.ibm.com>
2018-11-21 13:23:30 +0000

    add text

--- pkgbuild/pkgbuild.sh
+++ pkgbuild/pkgbuild.sh
@@ -58,7 +58,17 @@ cat distribution.xml.tmpl  \
   | sed -E "s/\\{file\\}/OpenJDK.pkg/g" \
   >distribution.xml ; \
 
-/usr/bin/pkgbuild --root ${INPUT_DIRECTORY} --install-location /Library/Java/JavaVirtualMachines/adoptopenjdk-${MAJOR_VERSION}.jdk --identifier net.adoptopenjdk.${MAJOR_VERSION}.jdk --version ${FULL_VERSION} --sign "${SIGN}" OpenJDK.pkg
+  cat Resources/en.lproj/welcome.html.tmpl  \
+  | sed -E "s/\\{major_version\\}/$MAJOR_VERSION/g" \
+  | sed -E "s/\\{full_version\\}/$FULL_VERSION/g" \
+  >Resources/en.lproj/welcome.html ; \
+
+  cat Resources/en.lproj/conclusion.html.tmpl  \
+  | sed -E "s/\\{major_version\\}/$MAJOR_VERSION/g" \
+  | sed -E "s/\\{full_version\\}/$FULL_VERSION/g" \
+  >Resources/en.lproj/conclusion.html ; \
+
+  /usr/bin/pkgbuild --root ${INPUT_DIRECTORY} --install-location /Library/Java/JavaVirtualMachines/adoptopenjdk-${MAJOR_VERSION}.jdk --identifier net.adoptopenjdk.${MAJOR_VERSION}.jdk --version ${FULL_VERSION} --sign "${SIGN}" OpenJDK.pkg
 /usr/bin/productbuild --distribution distribution.xml --resources Resources  --sign "${SIGN}" --package-path OpenJDK.pkg ${OUTPUT_DIRECTORY}
 
 
