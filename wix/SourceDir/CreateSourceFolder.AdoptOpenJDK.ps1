8daa018c159078deba554f7aad52c03c7609298d
douph1 <45632394+douph1@users.noreply.github.com>
2019-02-21 16:07:08 +0100

    Correct openj9 folder path

--- wix/SourceDir/CreateSourceFolder.AdoptOpenJDK.ps1
+++ wix/SourceDir/CreateSourceFolder.AdoptOpenJDK.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem -Path .\ -Filter *.zip -Recurse -File -Name| ForEach-Object {
+Get-ChildItem -Path .\ -Filter *.zip -File -Name| ForEach-Object {
   $filename = [System.IO.Path]::GetFileName($_)
   $jdk_version_found = $filename -match "(?<jdk>OpenJDK\d+)"
   $jdk_version = $Matches.jdk
@@ -6,5 +6,25 @@ Get-ChildItem -Path .\ -Filter *.zip -Recurse -File -Name| ForEach-Object {
   $package_type = $Matches.package_type
   $platform_found = $filename -match "(?<platform>x86-32|x64)"
   $platform = $Matches.platform
-Expand-Archive -Path $filename -DestinationPath ".\$jdk_version\$package_type\$platform"
+
+  Expand-Archive -Force -Path $filename -DestinationPath ".\$jdk_version\$package_type\$platform"
+  Get-ChildItem -Directory ".\$jdk_version\$package_type\$platform" | where {$_ -match ".*_.*"} | ForEach {
+    $SourcePath = [System.IO.Path]::GetDirectoryName($_.FullName)
+    #echo "SourcePath: " $SourcePath
+    #echo "fullname: "$_.FullName
+    #echo "Name: " $_.Name
+    if ( $_.Name -Match "(.*)_(.*)-jre$" ) {
+        $NewName = $_.Name -replace "(.*)_(.*)$",'$1-jre'
+    } elseif ( $_.Name -Match "(.*)_(.*)$" ) {
+        $NewName = $_.Name -replace "(.*)_(.*)$",'$1'
+    }
+    
+    #echo "NewName: " $NewName
+    $Destination = Join-Path -Path $SourcePath -ChildPath $NewName
+    #echo "Destination: "$Destination
+    
+    echo Moving $_.FullName to $Destination
+    if (Test-Path $Destination) { Remove-Item $Destination -Recurse; }
+    Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 }
--- wix/SourceDir/CreateSourceFolder.OpenJDK11.ps1
+++ wix/SourceDir/CreateSourceFolder.OpenJDK11.ps1
@@ -16,6 +16,24 @@ ForEach ($url in $urls) {
   $platform = $Matches.platform
 
   Invoke-WebRequest -Uri $url -outfile $filename
-  Expand-Archive -Path $filename -DestinationPath ".\$jdk_version\$package_type\$platform"
+  Expand-Archive -Force -Path $filename -DestinationPath ".\$jdk_version\$package_type\$platform"
+  Get-ChildItem -Directory ".\$jdk_version\$package_type\$platform" | where {$_ -match ".*_.*"} | ForEach {
+    $SourcePath = [System.IO.Path]::GetDirectoryName($_.FullName)
+    #echo "SourcePath: " $SourcePath
+    #echo "fullname: "$_.FullName
+    #echo "Name: " $_.Name
+    if ( $_.Name -Match "(.*)_(.*)-jre$" ) {
+        $NewName = $_.Name -replace "(.*)_(.*)$",'$1-jre'
+    } elseif ( $_.Name -Match "(.*)_(.*)$" ) {
+        $NewName = $_.Name -replace "(.*)_(.*)$",'$1'
+    }
+    
+    #echo "NewName: " $NewName
+    $Destination = Join-Path -Path $SourcePath -ChildPath $NewName
+    #echo "Destination: "$Destination
+    
+    echo Moving $_.FullName to $Destination
+    if (Test-Path $Destination) { Remove-Item $Destination -Recurse; }
+    Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 }
-
