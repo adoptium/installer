34aea4dbebe86399e33a76684937a5addcef0807
Andreas Ahlenstorf <aahlenst@users.noreply.github.com>
2020-03-18 17:47:31 +0100

    Add temporary workaround for OpenJ9 URL change (#198)

--- linux/standalone_create_installer_linux.sh
+++ linux/standalone_create_installer_linux.sh
@@ -47,7 +47,13 @@ shopt -s globstar nullglob nocaseglob nocasematch
 
 for DISTRIBUTION_TYPE in "jdk" "jre" ; do
     for ARCHITECTURE in "x64" "s390x" "ppc64le" "arm" "aarch64" ; do
-        JDK_FILENAME="OpenJDK${MAJOR_VERSION}U-${DISTRIBUTION_TYPE}_${ARCHITECTURE}_linux_${JVM}_${SUB_TAG}.tar.gz"
+        # Temporary workaround for release of 14+36
+        if [ "$JVM" == "hotspot" ] ; then
+            JDK_FILENAME="OpenJDK${MAJOR_VERSION}U-${DISTRIBUTION_TYPE}_${ARCHITECTURE}_linux_${JVM}_${SUB_TAG}.tar.gz"
+        else
+            JDK_FILENAME="OpenJDK${MAJOR_VERSION}-${DISTRIBUTION_TYPE}_${ARCHITECTURE}_linux_${JVM}_${SUB_TAG}.tar.gz"
+        fi
+
         DOWNLOAD_URL="https://github.com/AdoptOpenJDK/openjdk${MAJOR_VERSION}-binaries/releases/download/${TAG}/${JDK_FILENAME}"
 
         # Script should continue even if the file cannot be downloaded because
