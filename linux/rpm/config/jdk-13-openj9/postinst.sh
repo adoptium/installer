2c144f04e1eb820b75f51ed6590b6269f78939f5
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2020-07-05 14:57:36 +0200

    Fix alternatives' priorities on RPM-based systems
    JDK 12+ all had the same priority: 1121. As a result, installing a newer JDK would not replace the old one. The priorities are now as follows:
    
    * 12: 1121
    * 13: 1131
    * 14: 1141
    * 15: 1151

--- linux/rpm/config/jdk-13-openj9/postinst.sh
+++ linux/rpm/config/jdk-13-openj9/postinst.sh
@@ -1,5 +1,5 @@
 if [ $1 -ge 1 ] ; then
-    update-alternatives --install /usr/bin/java java {{ prefix }}/{{ jdkDirectoryName }}/bin/java 1121 \
+    update-alternatives --install /usr/bin/java java {{ prefix }}/{{ jdkDirectoryName }}/bin/java 1131 \
                         --slave /usr/bin/jfr jfr {{ prefix }}/{{ jdkDirectoryName }}/bin/jfr \
                         --slave /usr/bin/jjs jjs {{ prefix }}/{{ jdkDirectoryName }}/bin/jjs \
                         --slave /usr/bin/jrunscript jrunscript {{ prefix }}/{{ jdkDirectoryName }}/bin/jrunscript \
@@ -19,7 +19,7 @@ if [ $1 -ge 1 ] ; then
                         --slave /usr/share/man/man1/rmiregistry.1 rmiregistry.1 {{ prefix }}/{{ jdkDirectoryName }}/man/man1/rmiregistry.1 \
                         --slave /usr/share/man/man1/unpack200.1 unpack200.1 {{ prefix }}/{{ jdkDirectoryName }}/man/man1/unpack200.1
 
-    update-alternatives --install /usr/bin/javac javac {{ prefix }}/{{ jdkDirectoryName }}/bin/javac 1121 \
+    update-alternatives --install /usr/bin/javac javac {{ prefix }}/{{ jdkDirectoryName }}/bin/javac 1131 \
                         --slave /usr/bin/jar jar {{ prefix }}/{{ jdkDirectoryName }}/bin/jar \
                         --slave /usr/bin/jarsigner jarsigner {{ prefix }}/{{ jdkDirectoryName }}/bin/jarsigner \
                         --slave /usr/bin/javadoc javadoc {{ prefix }}/{{ jdkDirectoryName }}/bin/javadoc \
--- linux/rpm/config/jdk-15-openj9-jre/postinst.sh
+++ linux/rpm/config/jdk-15-openj9-jre/postinst.sh
@@ -1,5 +1,5 @@
 if [ $1 -ge 1 ] ; then
-    update-alternatives --install /usr/bin/java java {{ prefix }}/{{ jdkDirectoryName }}/bin/java 1121 \
+    update-alternatives --install /usr/bin/java java {{ prefix }}/{{ jdkDirectoryName }}/bin/java 1151 \
                         --slave /usr/bin/jfr jfr {{ prefix }}/{{ jdkDirectoryName }}/bin/jfr \
                         --slave /usr/bin/jjs jjs {{ prefix }}/{{ jdkDirectoryName }}/bin/jjs \
                         --slave /usr/bin/jrunscript jrunscript {{ prefix }}/{{ jdkDirectoryName }}/bin/jrunscript \
