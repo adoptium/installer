e0d0d2cb961ddc60e6130ef41951dfb34abf5248
George Adams <georgeadams1995@gmail.com>
2020-08-05 13:58:19 +0100

    fix

--- wix/SourceDir/InstallCodesignCert.AdoptOpenJDK.ps1
+++ wix/SourceDir/InstallCodesignCert.AdoptOpenJDK.ps1
@@ -1,4 +1,4 @@
 If (Test-Path -Path C:\Users\jenkins\windows.p12) {
     $client = new-object System.Net.WebClient
-    $client.DownloadFile("https://github.com/AdoptOpenJDK/openjdk-installer/raw/master/windows.p12", "C:\Users\jenkins\windows.p12")
+    $client.DownloadFile('https://github.com/AdoptOpenJDK/openjdk-installer/raw/master/windows.p12', 'C:\Users\jenkins\windows.p12')
 }
