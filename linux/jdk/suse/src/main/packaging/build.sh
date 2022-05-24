9ab0c3282813d6e35e36647834ab57bcd2e3b95f
Wen Zhou <wenzhou@redhat.com>
2022-05-24 10:55:16 +0200

    refact: move build.sh as file not generate everytime in docker build
    	- install tini from dnf, do not download it from source
    	- also remove install "sudo" which we only use gosu
    	- update: format RUN in dockerfile for readiness
    	- docs: update

---
+++ linux/jdk/suse/src/main/packaging/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Ensure necessary directories for rpmbuild operation are present.
+rpmdev-setuptree
+
+# Build all spec files we can find.
+
+targets="x86_64 ppc64le s390x aarch64 armv7hl"
+for spec in "$(ls /home/builder/build/generated/packaging/*.spec)"; do
+	rpmdev-spectool -g -R "$spec";
+	rpmbuild --nodeps -bs "$spec";
+	if [[ "$spec" =~ "temurin-8-jdk" ]]; then
+		targets="x86_64 ppc64le aarch64 armv7hl"
+	fi
+	for target in $targets; do
+		rpmbuild --target "$target" --rebuild /home/builder/rpmbuild/SRPMS/*.src.rpm;
+	done;
+done;
+
+# Copy generated RPMs to destination folder
+find /home/builder/rpmbuild/SRPMS /home/builder/rpmbuild/RPMS -type f -name "*.rpm" -print0 | xargs -0 -I {} cp {} /home/builder/out
+# Sign generated RPMs with rpmsign
+if grep -q %%_gpg_name /home/builder/.rpmmacros; then
+	rpmsign --addsign /home/builder/out/*.rpm
+fi;
