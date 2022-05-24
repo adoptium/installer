8f32c42859fbaf9beea111c5e308691f9d744926
Wen Zhou <wenzhou@redhat.com>
2022-05-24 15:02:26 +0200

    refact: remove "linux" which was for adoptopenjdk

--- linux/find_tools.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -eu
-
-for tool_path in $(find $1 -type f -executable) ; do
-    tool_dir_path=${tool_path%/*}
-    tool_dir=${tool_dir_path##*/}
-    echo $tool_dir/${tool_path##*/}
-done
--- linux/rpm/config/jdk-13-openj9-jre/prerm.sh
+++
@@ -1,3 +0,0 @@
-if [ $1 -eq 0 ]; then
-    update-alternatives --remove java {{ prefix }}/{{ jdkDirectoryName }}/bin/java
-fi
