92bede44ec9baa1d8f2408e9bb51e32c9e3568e2
George Adams <georgeadams1995@gmail.com>
2022-12-19 10:23:31 +0000

    linux: add support for Ubuntu Kinetic (#562)
    * \linux: add support for Ubuntu Kinetic
    
    * fixup
    
    * try fix
    
    * try this
    
    * add auto publish action
    
    * fix permissions
    
    * try now
    
    * Update Jenkinsfile
    
    Co-authored-by: Martijn Verburg <martijnverburg@gmail.com>

--- linux/jdk/alpine/src/main/packaging/entrypoint.sh
+++ linux/jdk/alpine/src/main/packaging/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-# The directory mounted into the container has the UID/GID of the host user. In order to allow the user builder to write
-# into it without changing its ownership (which could render the folder or its contents inaccessible to the host user),
-# add the user builder to the group with the same GID as the host user.
-HOST_USER_GID=$(stat -c "%g" /home/builder/out)
-getent group "$HOST_USER_GID" || (addgroup -g "$HOST_USER_GID" hostusrg && addgroup builder hostusrg)
-chmod g+w /home/builder/out
+# Copy build scripts into a directory within the container. Avoids polluting the mounted
+# directory and permission errors.
+mkdir /home/builder/workspace
+cp -R /home/builder/build/generated/packaging /home/builder/workspace
 
-# Drop root privileges and build the package.
-gosu builder /home/builder/build.sh
+# Set permssions
+sudo chown -R builder /home/builder/out
+
+# Build package and set distributions it supports
+cd /home/builder/workspace/packaging
+abuild -r
+
+# Copy resulting files into mounted directory where artifacts should be placed.
+mv /home/builder/packages/workspace/x86_64/*.{apk,tar.gz} /home/builder/out
--- linux/jdk/debian/src/main/packaging/build.sh
+++ linux/jdk/debian/src/main/packaging/build.sh
@@ -8,7 +8,7 @@ cp -R /home/builder/build/generated/packaging /home/builder/workspace
 
 
 # $ and $ARCH are env variables passing in from "docker run"
-debVersionList="buster bullseye bionic focal jammy"
+debVersionList="buster bullseye bionic focal jammy kinetic"
 
 # the target package is only based on the host machine's ARCH
 # ${buildArch} is only used for debug purpose what really matter is the label on the jenkins agent
