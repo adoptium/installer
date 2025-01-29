5ca028d63dc2f7bcd36f2af6f117b1f2cc1c9040
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2025-01-29 17:34:57 +0000

    Add files required to consistently build Alpine APKs for January Release (#1119)
    * Add Jan APK Updates
    
    * Fix APK package build
    
    * Update alpine for automation.
    
    * Update Opensuse/sles templates for JDK/JRE8

--- linux_new/jdk/alpine/src/main/packaging/entrypoint.sh
+++ linux_new/jdk/alpine/src/main/packaging/entrypoint.sh
@@ -6,11 +6,19 @@ set -euox pipefail
 mkdir /home/builder/workspace
 cp -R /home/builder/build/generated/packaging /home/builder/workspace
 
+# Install Adoptium Public Key
+sudo chmod 664 /etc/apk/repositories
+sudo chgrp abuild /etc/apk/repositories
+sudo wget -O /etc/apk/keys/adoptium.rsa.pub https://packages.adoptium.net/artifactory/api/security/keypair/public/repositories/apk
+sudo echo 'https://packages.adoptium.net/artifactory/apk/alpine/main' >> /etc/apk/repositories
+sudo wget -O /home/builder/.abuild/adoptium.rsa.pub https://packages.adoptium.net/artifactory/api/security/keypair/public/repositories/apk
+
 # Set permssions
 sudo chown -R builder /home/builder/out
 
 # Build package and set distributions it supports
 cd /home/builder/workspace/packaging
+sudo apk update
 abuild -r
 
 arch=$(abuild -A)
