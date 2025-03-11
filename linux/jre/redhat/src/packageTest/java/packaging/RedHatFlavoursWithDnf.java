8496139e9532f1cbc36c42be6056b3e2c9c0db87
Zed Spencer-Milnes <zed@ziax.com>
2025-03-11 01:30:50 +0000

    Add CentOS (Stream) 9 and CentOS (Stream) 10 to build matrix

--- linux/jre/redhat/src/packageTest/java/packaging/RedHatFlavoursWithDnf.java
+++ linux/jre/redhat/src/packageTest/java/packaging/RedHatFlavoursWithDnf.java
@@ -23,10 +23,12 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 import java.util.stream.Stream;
 
 /**
- * Provides the name and versions of container images using Red Hat flavours that the packages should be tested against
+ * Provides the name and versions of container images using Red Hat flavours
+ * that the packages should be tested against
  * and that use dnf as package manager.
  * <p/>
- * {@link RedHatFlavoursWithYum} takes care of Red Hat flavours that use yum as package manager.
+ * {@link RedHatFlavoursWithYum} takes care of Red Hat flavours that use yum as
+ * package manager.
  *
  * @see RedHatFlavoursWithYum
  * @author Andreas Ahlenstorf
@@ -37,31 +39,33 @@ public class RedHatFlavoursWithDnf implements ArgumentsProvider {
 		/*
 		 * Rocky Linux (CentOS replacement): All supported versions
 		 * Oracle Linux: All supported versions until premier support runs out
-		 *     (https://www.oracle.com/a/ocom/docs/elsp-lifetime-069338.pdf)
+		 * (https://www.oracle.com/a/ocom/docs/elsp-lifetime-069338.pdf)
 		 * Amazon Linux2 does not support DNF but only Yum
 		 * ubi7 does not have DNF pre-installed, ubi-minimal has microdnf
 		 * ubi8 has DNF installed
 		 */
 
-		String containerRegistry = "";
+		String quayIoDefaultRegistry = "quay.io/";
+		String dockerDefaultRegistry = "";
 
-		if (System.getenv("containerRegistry") == null) { 
-			System.out.println("Using docker.io as the default container registry");
+		if (System.getenv("containerRegistry") == null) {
+			System.out.println("Using docker.io and quay.io as the default container registry");
 		} else {
-			containerRegistry = System.getenv("containerRegistry");
-			System.out.println("Using container registry: " + containerRegistry);
+			quayIoDefaultRegistry = dockerDefaultRegistry = System.getenv("containerRegistry");
+			System.out.println("Using container registry: " + dockerDefaultRegistry);
 		}
 
 		return Stream.of(
-			Arguments.of(containerRegistry + "rockylinux", "8"),
-			Arguments.of(containerRegistry + "fedora", "35"),
-			Arguments.of(containerRegistry + "fedora", "36"),
-			Arguments.of(containerRegistry + "fedora", "37"),
-			Arguments.of(containerRegistry + "fedora", "38"),
-			Arguments.of(containerRegistry + "fedora", "39"),
-			Arguments.of(containerRegistry + "redhat/ubi8", "latest"),
-			Arguments.of(containerRegistry + "redhat/ubi9", "latest"),
-			Arguments.of(containerRegistry + "oraclelinux", "8")
-		);
+				Arguments.of(quayIoDefaultRegistry + "centos/centos", "9"),
+				Arguments.of(quayIoDefaultRegistry + "centos/centos", "10"),
+				Arguments.of(dockerDefaultRegistry + "rockylinux", "8"),
+				Arguments.of(dockerDefaultRegistry + "fedora", "35"),
+				Arguments.of(dockerDefaultRegistry + "fedora", "36"),
+				Arguments.of(dockerDefaultRegistry + "fedora", "37"),
+				Arguments.of(dockerDefaultRegistry + "fedora", "38"),
+				Arguments.of(dockerDefaultRegistry + "fedora", "39"),
+				Arguments.of(dockerDefaultRegistry + "redhat/ubi8", "latest"),
+				Arguments.of(dockerDefaultRegistry + "redhat/ubi9", "latest"),
+				Arguments.of(dockerDefaultRegistry + "oraclelinux", "8"));
 	}
 }
